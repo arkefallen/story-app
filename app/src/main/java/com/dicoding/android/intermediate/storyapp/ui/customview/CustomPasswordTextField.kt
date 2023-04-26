20bec65f4b517adadf5860763f4f9aa4fd1b4a3b
arkefallen <rafa9011@gmail.com>
2023-04-26 13:13:45 +0700

    update: add custom password edit text format checker with minimum 8 characters

--- app/src/main/java/com/dicoding/android/intermediate/storyapp/ui/customview/CustomPasswordTextField.kt
+++ app/src/main/java/com/dicoding/android/intermediate/storyapp/ui/customview/CustomPasswordTextField.kt
@@ -1,59 +1,53 @@
 package com.dicoding.android.intermediate.storyapp.ui.customview
 
 import android.content.Context
-import android.graphics.drawable.Drawable
+import android.graphics.Canvas
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
+import com.dicoding.android.intermediate.storyapp.R
 
-class CustomPasswordTextField : AppCompatEditText, View.OnTouchListener {
+class CustomPasswordTextField : AppCompatEditText{
     private var textCounter : Int = 0
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        init()
+    }
 
     private fun init() {
         addTextChangedListener(
             object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    TODO("Not yet implemented")
+                override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    textCounter = count
+                    if (s != null) {
+                        if (s.length >= 0 && s.length < 8) {
+                            error = "Password minimal harus 8 karakter"
+                        } else {
+                            error = null
+                        }
+                    }
                 }
 
-                override fun afterTextChanged(p0: Editable?) {
-                    TODO("Not yet implemented")
-                }
+                override fun afterTextChanged(p0: Editable?) {}
             }
         )
     }
 
-    private fun showErrorText() {
-    }
-
-    private fun hideErrorText() {
-
-    }
-
-    private fun setEditTextDrawables(
-        startOfTheText: Drawable? = null,
-        topOfTheText: Drawable? = null,
-        endOfTheText: Drawable? = null,
-        bottomOfTheText: Drawable? = null
-    ) {
-
-    }
-    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
-        TODO("Not yet implemented")
-    }
 }
